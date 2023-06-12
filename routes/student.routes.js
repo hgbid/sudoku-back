1e8d618ffd316001897e94067968c7a34d9aa450
hgbid <106002556+hgbid@users.noreply.github.com>
2023-06-12 23:58:53 +0300

    add docker

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { addStudent } = require("../controllers/student.controllers/add");
-const { runCode } = require("../controllers/student.controllers/runCode");
+//const { runCode } = require("../controllers/student.controllers/runCode");
+const { runCode } = require("../controllers/student.controllers/TESTrunCode");
 
 router.post("/add", addStudent);
 router.post("/run", runCode);
