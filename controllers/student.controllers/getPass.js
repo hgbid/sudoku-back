2794619239e43b131e759e73d2a1143f1fc04415
hgbid <106002556+hgbid@users.noreply.github.com>
2023-07-25 15:13:31 +0300

    getpass added a max list

--- controllers/student.controllers/getPass.js
+++ controllers/student.controllers/getPass.js
@@ -5,10 +5,14 @@ exports.getPass = async (req, res) => {
   logger.info("get student called");
 
   try {
-    const task = req.query.task;
+    let tasks = req.query.task;
     const region = req.query.region;
-    if (!task) {
+    const max = req.query.max || 9;
+    if (!tasks) {
       return res.status(400).send("Missing task parameter");
+    }
+    if (!Array.isArray(tasks)) {
+      tasks = [tasks];
     }
 
     let students = await Student.find()
@@ -29,25 +33,29 @@ exports.getPass = async (req, res) => {
       );
     }
 
-    const result = students
-      .map((student) => {
-        const submit = student.sumbits.find(
-          (submit) =>
-            submit.task === task && submit.data.some((data) => data.pass)
-        );
-        if (!submit) return null;
+    const result = {};
+    tasks.forEach((task) => {
+      result[task] = students
+        .map((student) => {
+          const submit = student.sumbits.find(
+            (submit) =>
+              submit.task === task && submit.data.some((data) => data.pass)
+          );
+          if (!submit) return null;
 
-        const earliestPass = submit.data
-          .filter((data) => data.pass)
-          .sort((a, b) => new Date(a.date) - new Date(b.date))[0];
+          const earliestPass = submit.data
+            .filter((data) => data.pass)
+            .sort((a, b) => new Date(a.date) - new Date(b.date))[0];
 
-        return {
-          name: student.name,
-          city: student.instructor.city,
-          date: earliestPass.date,
-        };
-      })
-      .filter((student) => student !== null);
+          return {
+            name: student.name,
+            city: student.instructor.city,
+            date: earliestPass.date,
+          };
+        })
+        .filter((student) => student !== null)
+        .slice(0, max);
+    });
 
     logger.info("student responded successfully");
     logger.info(result);
